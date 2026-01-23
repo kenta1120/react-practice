6f5b85b6f04c87e1dd691cdb9953abf5a1464bc9
Ishii Kenta <ken11ta20ken@icloud.com>
2026-01-23 20:14:53 +0900

    JSXの式埋め込みとstyle属性、createElement属性を勉強

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,46 @@ function Header() {
 }
 
 export default function App() {
+  const tab = 2;
+  const dest = "https://react.dev";
+  const isDocs = true;
+  const props = {
+    color: 'White',
+    backgroundColor: 'Blue',
+    padding: 3
+  };
+  const title = '実践入門';
+
+  const mainElement = React.createElement(
+    "div",
+    { className: "main" },
+    React.createElement(
+      "p",
+      null,
+      `「${title}」(クリエイティブ)`
+    )
+  );
+
   return (
-    <div>
+    <>
       <Header />
       <h1>Appコンポーネント</h1>
       <p>ページ全体を構成するUI部品です</p>
-    </div>
+
+      <div className="field">
+        <label htmlFor="title">署名</label>
+        <input id="title" tabIndex={tab} />
+      </div>
+
+      <div className="main">
+        <p>「{title}」(クリエイティブ) </p>
+      </div>
+
+      {mainElement}
+
+      <a href={dest + (isDocs ? "/docs" : "")}>React本家サイト</a>
+
+      <p style={props}>プロジェクト</p>
+    </>
   );
 }
